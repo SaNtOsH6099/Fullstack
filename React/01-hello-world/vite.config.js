148deb09f2b993670902ac09c39d544547b497cc
Santosh Chandra <santoshchandra609@gmail.com>
2026-08-16 16:04:41 +0530

    Task24

--- React/01-hello-world/vite.config.js
+++ React/01-hello-world/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Fullstack/React/01-hello-world/',
+  base: '/Fullstack/React',
   plugins: [react()],
 })
